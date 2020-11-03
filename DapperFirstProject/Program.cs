f22c3214e1d307fd502e73f435546a70f594c840
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-11-03 11:39:15 +0300

    Dapper Insert Command Added

--- DapperFirstProject/Program.cs
+++ DapperFirstProject/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Data.SqlClient;
 
 namespace DapperFirstProject
@@ -16,16 +17,20 @@ namespace DapperFirstProject
         static void Main(string[] args)
         {
 
+            //var con = new SqlConnection();
+            //con.ConnectionString = "server=DESKTOP-3VB3SSC\\SQLEXPRESS;database=DAPPERDB;integrated security=true";
+            //var cmd = new SqlCommand();
+            //cmd.Connection = con;
+            //cmd.CommandType = System.Data.CommandType.Text;
+            //cmd.CommandText = "insert into PRODUCTS values('iphone6',5000,40)";
+            //con.Open();
+            //var detail=  cmd.ExecuteNonQuery();
+            //con.Close();
+            //Console.WriteLine("İşlem tamamlandı "+detail.ToString());
             var con = new SqlConnection();
             con.ConnectionString = "server=DESKTOP-3VB3SSC\\SQLEXPRESS;database=DAPPERDB;integrated security=true";
-            var cmd = new SqlCommand();
-            cmd.Connection = con;
-            cmd.CommandType = System.Data.CommandType.Text;
-            cmd.CommandText = "insert into PRODUCTS values('iphone6',5000,40)";
-            con.Open();
-            var detail=  cmd.ExecuteNonQuery();
-            con.Close();
-            Console.WriteLine("İşlem tamamlandı "+detail.ToString());
+
+            con.Execute("insert into products values('Klavye',20,100)");
         }
     }
 }
