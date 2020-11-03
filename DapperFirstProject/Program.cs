64fddefc5a3132d5d0e4292ac75b76a0103c3e3c
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-11-03 11:30:34 +0300

    Add project files.

---
+++ DapperFirstProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DapperFirstProject
+{
+    class Program
+    {
+//        GO
+//CREATE TABLE PRODUCTS(
+//Id int identity(1,1),
+//Name nvarchar(100),
+//Price decimal (18,3),
+//Stock int,
+//primary key(Id)
+//)
+        static void Main(string[] args)
+        {
+
+            var con = new SqlConnection();
+            con.ConnectionString = "server=DESKTOP-3VB3SSC\\SQLEXPRESS;database=DAPPERDB;integrated security=true";
+            var cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "insert into PRODUCTS values('iphone6',5000,40)";
+            con.Open();
+            var detail=  cmd.ExecuteNonQuery();
+            con.Close();
+            Console.WriteLine("İşlem tamamlandı "+detail.ToString());
+        }
+    }
+}
